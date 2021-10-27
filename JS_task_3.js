7eb835ebce0910578096f0207ec3c11f747cd053
Andrii <bezkrovnyyav@gmail.com>
2021-10-27 19:41:08 +0300

    Task 3 and 4 are finished

--- JS_task_3.js
+++ JS_task_3.js
@@ -7,5 +7,47 @@ console.log(`Last element of this array is ${lastElement([3, 55, -4, 21])}`);
 // following array into a string
 
 const arr = [ "This", "program", "create", "a string", "from", "array's", "elements"];
-const str_from_arr = arr.join(" ");
-console.log(str_from_arr);
+const strFromArr = arr.join(" ");
+console.log(strFromArr);
+
+/* 
+3. There are two arrays with individual values, write a
+JavaScript program to compute the sum of each individual
+index value from the given arrays and save them to third
+array
+*/
+
+const array1 = [1, 0, 2, 8, 4];
+const array2 = [3, 5, 6, 7];
+
+sumOfTwoArrays = (array1, array2) => {
+      let finalArray = [];
+      for (let elem = 0; elem < array1.length || elem < array2.length; elem ++) {
+          finalArray.push((array1[elem] || 0) + (array2[elem] || 0));
+      }
+      return finalArray
+  };
+
+let array3 = sumOfTwoArrays(array1, array2)
+console.log(`New array is ${array3}`);
+
+
+/*
+4. Write a JavaScript program to calculate the area and
+perimeter of a rectangle. Rectangle should be an object with
+properties width and height and methods getArea() and
+getPerimeter();
+*/
+
+let rectangle = {
+  width: 3,
+  height: 5,
+  getArea: function () {
+      return this.width * this.height;
+  },
+  getPerimeter: function () {
+      return (this.width * this.height) * 2;
+  }
+};
+console.log(`The area of a rectangle is ${rectangle.getArea()}`);
+console.log(`The perimeter of a rectangle is ${rectangle.getPerimeter()}`);
